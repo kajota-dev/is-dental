5e1f58cbfa5f0de55e591180ab4362c0e86e3eb1
Jefferson Lopez Porras <kajotadev@gmail.com>
2025-10-30 15:50:40 -0600

    agregar footer

--- src/app/(clinic)/layout.tsx
+++ src/app/(clinic)/layout.tsx
@@ -1,10 +1,11 @@
 import Navbar from "@/components/layout/Navbar"
-
+import Footer from "@/components/layout/footer"
 export default function ClinicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       {children}
+      <Footer/>
     </>
   )
 }
